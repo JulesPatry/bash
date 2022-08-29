903e8428240fb88e1c6329dd74ad9eb0e25e26bb
julespatry <julespatry@gmail.com>
2022-08-29 14:58:39 -0400

    updates user experiece on find-replace script with read prompt

--- find-replace.sh
+++ find-replace.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 exclude="node_modules"
-filesContainingText="testGroupType: 'sentry',"
+filesContainingText="type: 'universe',"
 oldText="status: 1,"
 newText="status: 2,"
 
-output=$(grep -HRl --exclude-dir="$exclude" --exclude=\*.sh "$filesContainingText" . | cut -d: -f1) 
+output=$(grep -HRl --exclude-dir="node_modules" --exclude-dir="./build" --exclude=\*.sh "$filesContainingText" . | cut -d: -f1) 
+targets=(${output// / })
+targetsLength=${#targets[@]}
+
+if [ -n "$output" ] 
+then 
+  echo ""
+  confirm=""
+  read -p $"You are about to change text on ${#targets[@]} file(s)... `echo $'\n '` ${oldText}`echo $'\n '` ${newText} `echo $'\n> '` Are you sure? (y) " confirm
+  if [ $confirm == "y" ] 
+  then 
+    for OUTPUT in ${targets[@]}
+    do
+      echo $OUTPUT
+      sed -i '' "s/$oldText/$newText/g" $OUTPUT
+    done
+  else 
+    echo "Canceled Find-Replace CMD"
+  fi 
+else 
+  echo "There are no files found with text`echo $'\n '`${filesContainingText}"
+fi 
 
 
-for OUTPUT in ${output// / }
-do
-    echo $OUTPUT
-    sed -i '' "s/$oldText/$newText/g" $OUTPUT
-done
+
---
+++ text.ts
@@ -0,0 +1,5 @@
+export default {
+  hello: 'world',
+  type: 'universe',
+  status: 2,
+};
